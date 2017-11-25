916953ccc80a16931887a1322df9c7ad3872ffc5
Brian Loi <31304500+brianloi@users.noreply.github.com>
2017-11-24 21:37:23 -0700

    Init ISTA130 dir

---
+++ ISTA130/template.py
@@ -0,0 +1,27 @@
+'''
+Author: <your name>
+Date: <the date>
+Class: ISTA 130
+Section Leader: <your section leader>
+
+Description:
+<summary of this program>
+'''
+
+# put all of your import statements below this line and then delete this comment
+
+# put all of your function definitions below this line and then delete this comment
+
+
+#==========================================================
+def main():
+    '''
+    Write a description of what happens when you run
+    this file here.
+    '''
+    # put main code here, make sure each line is indented one level, and delete this comment
+
+    turtle.getscreen().exitonclick()  # keeps the turtle graphics window open
+
+if __name__ == '__main__':
+    main()
